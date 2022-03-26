f1ec53c4a34480ae343f681e2ce442fdc2316145
unknown <okwuosahpaschal@gmail.com>
2022-03-26 22:26:26 +0100

    horror commit

--- DocPages/Horror.js
+++ DocPages/Horror.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Code = () => {
-  return `fetch('/api/getHorrorMovies')
+  return `fetch('https://openmdb.netlify.app/api/getHorrorMovies')
   .then(function(response) {
     return response.json();
   }).then(function(data) {
@@ -34,7 +34,7 @@ export default function Horror() {
       <h1 className="font-extrabold text-2xl">Get Horror Movies</h1>
 
       <p className="font-medium mt-4">the API EndPoint</p>
-      <p className="font-medium mt-2">http://localhost:3000/api/getHorrorMovies</p>
+      <p className="font-medium mt-2">https://openmdb.netlify.app/api/getHorrorMovies</p>
       <div>
         <p className="font-semibold mt-4">Code example</p>
         <div className="bg-[#04111d] w-[100%] codePadding mt-4">
